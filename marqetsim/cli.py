3f2b85b3365702bcd38384b835dc94fee179d4c0
Andreas Chandra <phoenix.ichigo@yahoo.com>
2025-07-20 19:58:29 +0700

    #12 add generated agent feature

--- marqetsim/cli.py
+++ marqetsim/cli.py
@@ -7,6 +7,7 @@ import click
 from marqetsim.examples import create_joe_the_analyst
 from marqetsim.simulator import create_person
 from marqetsim.utils import common
+from marqetsim.agent_generator import generate_coherent_person
 
 
 @click.command()
@@ -19,6 +20,7 @@ def launch(file_path):
         options = data.pop("options")
         questions = data.pop("questions")
 
+        people = []
         if "agent" not in data:
             print("agent is not defined, use predefined agent Joe the Analyst")
             people = [create_joe_the_analyst()]
@@ -31,6 +33,11 @@ def launch(file_path):
                 assert agent_file_path.is_file(), "Agent file does not exist."
                 data_agent = common.read_csv(agent_file_path)
                 people = [create_person(profile=profile) for profile in data_agent]
+            elif isinstance(data["agent"], int):
+                for i in range(data["agent"]):
+                    record = generate_coherent_person()
+                    person = create_person(record)
+                    people.append(person)
             else:
                 raise ValueError(
                     "Invalid agent definition. Must be a dict or a file path."
