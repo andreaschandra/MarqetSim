f01abb778bd508430d48a5775810660d9f8e2ec5
Andreas Chandra <phoenix.ichigo@yahoo.com>
2025-08-10 13:31:01 +0700

    update environment

--- marqetsim/environment.py
+++ marqetsim/environment.py
@@ -7,4 +7,13 @@ class Environment:
     """General environment settings for MarqetSim."""
 
     def __init__(self):
-        self.current_datetime = datetime.now()
+        self.world = {}
+        self.world["current_datetime"] = datetime.now()
+
+    def set_param(self, key, value):
+        """Set a parameter in the environment."""
+        self.world[key] = value
+
+    def get_param(self, key):
+        """Get a parameter from the environment."""
+        return self.world.get(key, None)
